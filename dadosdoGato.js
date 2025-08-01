6737be20ebf4464222accee462ac27917c60eb00
Dayane kelly <54118755+daykell1@users.noreply.github.com>
2025-08-01 16:53:04 -0300

    colocando cor no nome do gato para ficar em destaque

--- dadosdoGato.js
+++ dadosdoGato.js
@@ -59,21 +59,22 @@ function leridade() {
 
 function lerpeso() {
     peso = document.getElementById("peso").value;
-    document.getElementById("titulodados").innerHTML = ``;        
+    document.getElementById("titulodados").innerHTML = ``;    
+     let nome1 = `<span class="nome-destaque">${nome}</span>`; 
     if (idade >= 1 && idade <= 7 ){
         if (sexo == "MACHO" || sexo == "M"){
-            if (peso >= 4 && peso <=6) document.getElementById("campopeso").innerHTML = `<h1 class="titulo-text" id = "campopeso">${nome} está na faixa normal de peso</h1>`;
-            else if (peso < 4) document.getElementById("campopeso").innerHTML = `<h1 class="titulo-text" id = "campopeso">${nome} está abaixo do peso</h1>`;
-            else if (peso > 6) document.getElementById("campopeso").innerHTML = `<h1 class="titulo-text" id = "campopeso">${nome} está acima do peso</h1>`;
+            if (peso >= 4 && peso <=6) document.getElementById("campopeso").innerHTML = `<h1 class="titulo-text" id = "campopeso">${nome1} está na faixa normal de peso</h1>`;
+            else if (peso < 4) document.getElementById("campopeso").innerHTML = `<h1 class="titulo-text" id = "campopeso">${nome1} está abaixo do peso </h1>`;
+            else if (peso > 6) document.getElementById("campopeso").innerHTML = `<h1 class="titulo-text" id = "campopeso">${nome1} está acima do peso</h1>`;
         }else if(sexo == "FÊMEA" || sexo == "FEMEA" || sexo == "F"){
-            if (peso >= 3 && peso <=5) document.getElementById("campopeso").innerHTML = `<h1 class="titulo-text" id = "campopeso">${nome} está na faixa normal de peso</h1>`;        
+            if (peso >= 3 && peso <=5) document.getElementById("campopeso").innerHTML = `<h1 class="titulo-text" id = "campopeso">${nome1} está na faixa normal de peso</h1>`;        
             else if (peso < 3) document.getElementById("campopeso").innerHTML = `<h1 class="titulo-text" id = "campopeso">${nome} está abaixo do peso</h1>`;
-            else if (peso > 5) document.getElementById("campopeso").innerHTML = `<h1 class="titulo-text" id = "campopeso">${nome} está acima do peso</h1>`;
+            else if (peso > 5) document.getElementById("campopeso").innerHTML = `<h1 class="titulo-text" id = "campopeso">${nome1} está acima do peso</h1>`;
         }
     }else{
         document.getElementById("campopeso").innerHTML = `<h1 class="titulo-text" id = "campopeso">Não temos avaliação para peso ideal nessa faixa de idade</h1>`;
     }
 
-    document.getElementById("campopeso").innerHTML += `<img id="gatosPeso" src="img/gatosPeso.png" alt="gatosPeso">`
+    // document.getElementById("campopeso").innerHTML += `<img id="gatosPeso" src="img/gatosPeso.png" alt="gatosPeso">`
     
 }
